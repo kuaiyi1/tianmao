38b5fead877c7af9a33aaa49cfbe4886f868df73
kuaiyi1 <3192261624@qq.com>
2018-03-28 08:40:50 +0800

    i

---
+++ js/index.js
@@ -0,0 +1,32 @@
+{
+	// var t=setInterval(move,3000);
+	// var n=0;
+	// function move(){
+	// 	n++;
+	// 	if(n==$(".banner_bottom_img").length){
+	// 		n=0;
+	// 	}
+	// 	if(n<0){
+	// 		n=$(".banner_bottom_img").length-1;
+	// 	}
+	// 	$(".banner_bottom_img").removeClass('active').eq(n).addClass('active');
+	// 	$(".banner_nav_list").removeClass('action').eq(n).addClass("action");
+	// }
+	// console.log($(".banner_nav_list"));
+	
+	// $(".banner_bottom1").mouseenter(function(){
+		
+	// 	clearInterval(t);
+	// });
+	// $(".banner_bottom1").mouseleave(function(){
+	// t=setInterval(move, 3000);
+	// });
+
+	// $(".banner_nav_list").mouseenter(function(index,ele){
+	// 	console.log(1);
+	// 	$(".banner_nav_list").removeClass("action").$(index).adsdClass("action");
+	// 	$(".banner_bottom_img").removeClass('active').eq(index).addClass('active');
+
+
+	// })
+}
